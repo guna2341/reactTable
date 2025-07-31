72ab0108735304f758c780ab385e4d0fc49d7da0
Guna_Nihil <92043445+guna2341@users.noreply.github.com>
2025-07-31 20:58:19 +0530

    Delete frontend directory

--- frontend/src/zustand/admin/index.jsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { activeStudents, dashboardStats, learningUnitsLength, pendingReviews, recentActivies, totalQuestions } from "../../utils/admin";
-
-
-export const useAdminStore = create((set, get) => ({
-    stats:dashboardStats,
-    recentActivities:recentActivies
-}));
